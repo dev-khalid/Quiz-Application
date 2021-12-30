52d82eaa1026b358a7deb0b3b0bc55086ef95ac2
dev-khalid <khalid.hossain.akash@gmail.com>
2021-12-30 21:01:57 +0600

    First work

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/App.css'
+const App = () => {
+  return (
+    <div>
+      hello from frontend
+    </div>
+  )
+}
+//now it's time to organize frontend.
+
+
+export default App
---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import classes  from '../styles/Nav.module.css'; 
+import Account from './Account'; 
+import logo from '../assets/images/logo-bg.png'; 
+
+const Nav = () => {
+  return (
+     <nav className="nav">
+      <ul>
+        <li>
+          <a href="index.html" className={classes.brand}>
+            <img src={logo} alt="Learn with Sumit Logo" />
+            <h3>Learn with Sumit</h3>
+          </a>
+        </li>
+      </ul>
+      <Account />
+    </nav>
+
+  )
+}
+
+export default Nav
