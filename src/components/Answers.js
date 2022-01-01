2ec9c33b021a2d96233c262eef8897a3c0662272
dev-khalid <khalid.hossain.akash@gmail.com>
2022-01-01 11:13:32 +0600

    Quiz and Result page done

---
+++ src/components/Answers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from '../styles/Ansers.module.css';
+import Checkbox from './Checkbox'; 
+const Answers = () => {
+  return (
+    <div className={classes.answers}>
+      <Checkbox className={classes.answer} text='Test Answer' />
+    </div>
+  )
+}
+
+export default Answers
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import '../styles/App.css';
-import Layout from './Layout'; 
-import Signup from './pages/Signup'; 
-import Home from './pages/Home'; 
-import Login from './pages/Login'
+import Layout from './Layout';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Quiz from './pages/Quiz';
+import Result from './pages/Result'; 
 const App = () => {
   return (
     <Layout>
       {/* <Home /> */}
       {/* <Signup /> */}
-      <Login />
+      {/* <Login /> */}
+      {/* <Quiz /> */}
+      <Result />
     </Layout>
   );
 };
