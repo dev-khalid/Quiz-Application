35306d15a2bb451c8624bb2b3687aca3e23cd279
dev-khalid <khalid.hossain.akash@gmail.com>
2022-01-03 07:50:43 +0600

    Complete login signup logout system with firebase

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,20 +1,64 @@
-import React from 'react'
-import Form from './Form'
-import Button from './Button'
-import TextInput from './TextInput'
-import { NavLink } from 'react-router-dom'
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import Button from './Button';
+import Form from './Form';
+import TextInput from './TextInput';
 
-const LoginForm = () => {
+export default function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
+
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      setError('');
+      setLoading(true);
+      await login(email, password);
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+      setError('Failed to login!');
+    }
+  }
+
   return (
-    <Form style={{ height: '330px' }}>
-      <TextInput type="text" placeholder="Enter Email" icon="alternate_email" />
-      <TextInput type="password" placeholder="Enter Password" icon="lock" />
-      <Button>Submit Now</Button>
-      <div class="info">
+    <Form style={{ height: '330px' }} onSubmit={handleSubmit}>
+      <TextInput
+        type="text"
+        placeholder="Enter email"
+        icon="alternate_email"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <TextInput
+        type="password"
+        placeholder="Enter password"
+        icon="lock"
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <Button type="submit" disabled={loading}>
+        <span>Submit Now</span>
+      </Button>
+
+      {error && <p className="error">{error}</p>}
+
+      <div className="info">
         Don't have an account? <NavLink to="/signup">Signup</NavLink> instead.
       </div>
     </Form>
   );
 }
-
-export default LoginForm
